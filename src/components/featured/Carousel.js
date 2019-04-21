cf33e21534c400960fd9101d962efb84433f89ce
umairx97 <umair.bajwa@hotmail.com>
2019-04-21 23:05:27 +0500

    Completed Carousel

--- src/components/featured/Carousel.js
+++ src/components/featured/Carousel.js
@@ -1,6 +1,10 @@
 import React from "react";
 import Slider from "react-slick";
 
+import slide_one from "../../resources/images/slide_one.jpg";
+import slide_two from "../../resources/images/slide_two.jpg";
+import slide_three from "../../resources/images/slide_three.jpg";
+
 const Carousel = () => {
   const settings = {
     dots: false,
@@ -9,11 +13,45 @@ const Carousel = () => {
     speed: 500
   };
   return (
-    <div className="carrousel_wrapper" style = {{
+    <div
+      className="carrousel_wrapper"
+      style={{
+        backgroundColor: "red",
         height: `${window.innerHeight}px`,
-        overflow: 'hidden'
-    }}>
-      <Slider {...settings} />
+        overflow: "hidden"
+      }}
+    >
+      <Slider {...settings}>
+        <div>
+          <div
+            className="carrousel_image"
+            style={{
+              background: `url(${slide_one})`,
+              height: `${window.innerHeight}px`
+            }}
+          />
+        </div>
+
+        <div>
+          <div
+            className="carrousel_image"
+            style={{
+              background: `url(${slide_two})`,
+              height: `${window.innerHeight}px`
+            }}
+          />
+        </div>
+
+        <div>
+          <div
+            className="carrousel_image"
+            style={{
+              background: `url(${slide_three})`,
+              height: `${window.innerHeight}px`
+            }}
+          />
+        </div>
+      </Slider>
     </div>
   );
 };
