061a172e2674ecfd8193a3b753ffa59ce75df3a2
umairx97 <umair.bajwa@hotmail.com>
2019-04-21 19:42:33 +0500

    Added Header

--- src/components/header_footer/Header.js
+++ src/components/header_footer/Header.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import Toolbar from "@material-ui/core/Toolbar";
 import AppBar from "@material-ui/core/AppBar";
-import MenuIcon from "@material-ui/core/Menu";
+import MenuIcon from "@material-ui/icons/Menu";
 import IconButton from "@material-ui/core/IconButton";
 
 class Header extends Component {
@@ -21,6 +21,14 @@ class Header extends Component {
               <div className="font_righteous header_logo_venue">The Venue</div>
               <div className="header_logo_title">Musical Events</div>
             </div>
+
+            <IconButton
+              aria-label="Menu"
+              color="inherit"
+              onClick={() => console.log("Opening Drawer")}
+            >
+              <MenuIcon />
+            </IconButton>
           </Toolbar>
         </AppBar>
       </div>
