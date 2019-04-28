f27843d05dbbe2bfba98708f05f3f1b2fe506638
umairx97 <umair.bajwa@hotmail.com>
2019-04-28 22:55:16 +0500

    Completed pricing section

--- src/App.js
+++ src/App.js
@@ -4,17 +4,17 @@ import Header from "./components/header_footer/Header";
 import Featured from "./components/featured";
 import VenueNfo from "./components/VenueNfo";
 import Highlights from "./components/Highlights";
+import Pricing from "./components/Pricing";
+
 class App extends Component {
   render() {
     return (
-      <div
-        className="App"
-        style={{ height: "1500px" }}
-      >
+      <div className="App" style={{ height: "1500px" }}>
         <Header />
         <Featured />
         <VenueNfo />
         <Highlights />
+        <Pricing />
       </div>
     );
   }
